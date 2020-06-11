55ef1d4c6755b99d702df6cab60037ffe979db46
xamantra <xamantrx@gmail.com>
2020-06-11 15:16:55 +0800

    scrollable settings

--- lib/src/pages/settings.dart
+++ lib/src/pages/settings.dart
@@ -49,68 +49,71 @@ class _SettingsState extends MomentumState<Settings> {
                 controllers: [SettingsController],
                 builder: (context, snapshot) {
                   var setting = snapshot<SettingsModel>();
-                  return Column(
-                    children: [
-                      BoolSetting(
-                        title: 'Draft inputs',
-                        description: 'When pressing "+" button inside home page, the previous inputs will be shown on Add List Page including edit session.',
-                        isChecked: setting.draftInputs,
-                        onChanged: (state) {
-                          setting.controller.setDraftInputs(state);
-                        },
-                      ),
-                      BoolSetting(
-                        title: 'Clear on Add',
-                        description: 'After adding new item on the list, the item name textbox will be cleared.',
-                        isChecked: setting.clearOnAdd,
-                        onChanged: (state) {
-                          setting.controller.setClearOnAdd(state);
-                        },
-                      ),
-                      BoolSetting(
-                        title: 'Copy List Name',
-                        description: 'When copying a list data, the list\'s name will also be copied.',
-                        isChecked: setting.copyListName,
-                        onChanged: (state) {
-                          setting.controller.setCopyListName(state);
-                        },
-                      ),
-                      BoolSetting(
-                        title: 'Copy List States',
-                        description: 'When copying a list data, the checkbox states will also be copied.',
-                        isChecked: setting.copyListStates,
-                        onChanged: (state) {
-                          setting.controller.setCopyListStates(state);
-                        },
-                      ),
-                      ActionSetting(
-                        title: 'Clear Draft',
-                        description: 'Clear draft inputs inside "Add New List" page.',
-                        action: () {
-                          setting.controller.clearDraft();
-                        },
-                      ),
-                      ActionSetting(
-                        title: 'Select Theme',
-                        description: 'An app without a theme switcher isn\'t an app at all.',
-                        action: () {
-                          showDialog(
-                            context: context,
-                            builder: (context) => ThemeSelector(),
-                          );
-                        },
-                      ),
-                      ActionSetting(
-                        title: 'Restart App',
-                        description: 'Clear input draft, reset navigation history and go to home page.',
-                        action: () async {
-                          await Router.resetWithContext<Home>(context);
-                          Momentum.controller<InputController>(context).reset(clearHistory: true);
-                          Momentum.controller<ListController>(context).clearSearch();
-                          Momentum.restart(context, momentum());
-                        },
-                      ),
-                    ],
+                  return SingleChildScrollView(
+                    physics: BouncingScrollPhysics(),
+                    child: Column(
+                      children: [
+                        BoolSetting(
+                          title: 'Draft inputs',
+                          description: 'When pressing "+" button inside home page, the previous inputs will be shown on Add List Page including edit session.',
+                          isChecked: setting.draftInputs,
+                          onChanged: (state) {
+                            setting.controller.setDraftInputs(state);
+                          },
+                        ),
+                        BoolSetting(
+                          title: 'Clear on Add',
+                          description: 'After adding new item on the list, the item name textbox will be cleared.',
+                          isChecked: setting.clearOnAdd,
+                          onChanged: (state) {
+                            setting.controller.setClearOnAdd(state);
+                          },
+                        ),
+                        BoolSetting(
+                          title: 'Copy List Name',
+                          description: 'When copying a list data, the list\'s name will also be copied.',
+                          isChecked: setting.copyListName,
+                          onChanged: (state) {
+                            setting.controller.setCopyListName(state);
+                          },
+                        ),
+                        BoolSetting(
+                          title: 'Copy List States',
+                          description: 'When copying a list data, the checkbox states will also be copied.',
+                          isChecked: setting.copyListStates,
+                          onChanged: (state) {
+                            setting.controller.setCopyListStates(state);
+                          },
+                        ),
+                        ActionSetting(
+                          title: 'Clear Draft',
+                          description: 'Clear draft inputs inside "Add New List" page.',
+                          action: () {
+                            setting.controller.clearDraft();
+                          },
+                        ),
+                        ActionSetting(
+                          title: 'Select Theme',
+                          description: 'An app without a theme switcher isn\'t an app at all.',
+                          action: () {
+                            showDialog(
+                              context: context,
+                              builder: (context) => ThemeSelector(),
+                            );
+                          },
+                        ),
+                        ActionSetting(
+                          title: 'Restart App',
+                          description: 'Clear input draft, reset navigation history and go to home page.',
+                          action: () async {
+                            await Router.resetWithContext<Home>(context);
+                            Momentum.controller<InputController>(context).reset(clearHistory: true);
+                            Momentum.controller<ListController>(context).clearSearch();
+                            Momentum.restart(context, momentum());
+                          },
+                        ),
+                      ],
+                    ),
                   );
                 },
               ),
